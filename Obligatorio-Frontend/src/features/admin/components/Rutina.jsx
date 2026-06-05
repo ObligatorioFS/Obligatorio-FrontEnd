c2b4eeeb9c1fde07172e2c343e28d7cfd4f56494
Feliperossini <FR337179@fi365.ort.edu.uy>
2026-06-05 19:53:17 -0300

    modificando rutinas

--- Obligatorio-Frontend/src/features/admin/components/Rutina.jsx
+++ Obligatorio-Frontend/src/features/admin/components/Rutina.jsx
@@ -1,46 +1,130 @@
-/*import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
 import MensajeAlerta from "../../shared/components/MensajeAlerta"
 import { useEffect, useRef, useState } from "react"
 import { actualizarActividad, eliminarActividad } from "../../actividadesSlice"
 import { useNavigate } from "react-router"
 
-const Rutina = ({rutina}) => {
-    
+const Rutina = ({ rutina }) => {
 
-    if (!editando) {
+  const dispatch = useDispatch()
+  const [editando, setEditando] = useState(false)
+  const [mensaje, setMensaje] = useState("")
+  const [mensajeExito, setMensajeExito] = useState("")
+  const [cargando, setCargando] = useState(false)
+  const navigate = useNavigate()
+  const inputEjercicioRef = useRef()
+
+  useEffect(() => {
+  if (!mensaje && !mensajeExito) return
+  const timeoutId = setTimeout(() => {
+    setMensaje("")
+    setMensajeExito("")
+  }, 3000)
+  return () => clearTimeout(timeoutId)
+  }, [mensaje, mensajeExito])
+
+
+  /* /rutinas/:id     PUT*/
+
+  const handleOnClickEditar = () => {
+    setMensaje("")
+    setEditando(true)
+  }
+
+  const handleOnClickAgregarEjercicio = () => {
+    const ejercicio = inputEjercicioRef.current.value
+
+    if (!ejercicio) {
+      setMensaje("El campo ejercicio es obligatorio")
+      return
+    }
+    setCargando(true)
+
+    fetch(`https://obligatorio-full-stack-ecru.vercel.app/v1/rutinas/${actividad._id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token")
+      },
+      body: JSON.stringify({ nombre, descripcion })
+    })
+      .then(async res => {
+        if (res.ok) {
+          return res.json()
+        }
+        if (res.status === 401) {
+        localStorage.removeItem("token")
+        navigate("/login")
+        return
+        }
+        const error = await res.json()
+        throw new Error(error.message || 'No se pudo modificar la sala')
+      })
+      .then(() => {
+        dispatch(actualizarActividad({
+          id: actividad._id,
+          modificado: { nombre, descripcion }
+        }))
+        setMensaje("")
+        setMensajeExito("Actividad modificada correctamente")
+        setEditando(false)
+      })
+      .catch(error => {
+        setMensaje(error.message)
+      }).finally(() => setCargando(false))
+  }
+
+  }
+
+  const handleOnClickGuardarEdicion = () => {
+    setCargando(true)
+  }
+
+  if (!editando) {
     return (
-      <div class="rutina-card">
-
-  <h2 class="rutina-objetivo">
-    Ganar masa muscular
-  </h2>
-
-  <p class="rutina-info">
-    <strong>Actividad:</strong> Gimnasio
-  </p>
-
-  <p class="rutina-info">
-    <strong>Para:</strong> usuario@email.com
-  </p>
-
-  <button class="rutina-btn">
-    Ver ejercicios
-  </button>
-
-</div>
+      <div className="quick-item">
+        <h2>
+          {rutina.objetivo}
+        </h2>
+        <p>
+          <strong>Actividad:</strong> {rutina.actividad.nombre}
+        </p>
+        <p >
+          <strong>Para:</strong> {rutina.usuario.email}
+        </p>
+        <button onClick={handleOnClickEditar} className="table-btn icon-btn edit-btn" aria-label="Editar actividad">
+          Editar
+        </button>
+        <MensajeAlerta mensaje={mensaje} flotante />
+        <MensajeAlerta mensaje={mensajeExito} tipo="exito" flotante />
+      </div>
     )
   } else {
     return (
       <div className="quick-item">
         <div>
-          <input ref={inputNombreRef} defaultValue={actividad.nombre} />
-          <textarea ref={inputDescripcionRef} defaultValue={actividad.descripcion} />
+          <h2>{rutina.objetivo}</h2>
+          <p>
+            <strong>Actividad:</strong> {rutina.actividad.nombre}
+          </p>
+          <p>
+            <strong>Para:</strong> {rutina.usuario.email}
+          </p>
+        </div>
+        <div>
+          <label>
+            <strong>Ejercicios:</strong>
+          </label>
+          <input type="text" placeholder="Ingrese la rutina" />
           <MensajeAlerta mensaje={mensaje} />
         </div>
-        <button onClick={handleOnClickGuardarEdicion} className="table-btn save-btn">Guardar</button>
+        <button onClick={handleOnClickAgregarEjercicio} className="table-btn save-btn" disabled={cargando}>
+          {cargando ? "Agregando ejercicio..." : "Agrregar ejercicios"}
+        </button>
+        <button onClick={handleOnClickGuardarEdicion} className="table-btn save-btn" disabled={cargando}>
+          {cargando ? "Guardando..." : "Guardar"}
+        </button>
       </div>
     )
   }
-
-
-} */
+}
